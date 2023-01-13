3e5a1617102ebbcca7d606918045ee7a8bd9b01e
unknown <getmanoa@gmail.com>
2023-01-13 13:22:30 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { data } from './data';
 
 function App() {
+  const [gifts, setGifts] = useState (data);
+  const removeGift = (id) => {
+    let newGift = gifts.filter(gift => gift.id !==id)
+    setGifts(newGift)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='container'>
+        <h1>Список из {gifts.length} вещей на День рождения</h1>
+      </div>
+
+      {gifts.map((element => {
+        const {id, gift, image} = element;
+
+        return (
+          <div>
+            <div className='container'>
+              <h2>{id} {gift}</h2>
+              <img src={image} width='400px' alt='ph'/>
+            </div>
+            <div className='container'>
+              <button className='btn' onClick={() => removeGift(id)}>УДАЛИТЬ</button>
+            </div>
+          </div>
+        )
+      }))}
+            <div className='container'>
+              <button className='btnTwo' onClick={() => setGifts([])}>УДАЛИТЬ ВСЁ</button>
+            </div>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,31 @@
+export const data = [
+    {
+    id: 1,
+    gift: "Кроссовки Reebok",
+    image: "https://www.shoes-report.ru/upload/iblock/63d/Reebok_Tom_Jerry_Global_Social_Post_FW4011_Closeup_Badge_Right_1080x1080px.jpg"
+    },
+
+    {
+    id: 2,
+    gift: "Louis Vuitton",
+    image: "https://static.insales-cdn.com/r/B9BNxm6mxNY/rs:fit:1000:0:1/q:100/plain/images/products/1/3544/563383768/588DA264-27EE-44DA-ADA7-4786C549333E.jpeg"
+    },
+
+    {
+    id: 3,
+    gift: "Montale духи",
+    image: "https://www.konfik.ru/wa-data/public/photos/13/10/1013/1013.970.png"
+    }, 
+
+    {
+    id: 4,
+    gift: "Ремень GUCCI",
+    image: "https://cdn1.ozone.ru/s3/multimedia-s/c1000/6356211352.jpg"
+    },
+    
+    {
+    id: 5,
+    gift: "Туфли JIMMY CHOO",
+    image: "https://weddingshoes.com.ua/wp-content/uploads/2021/03/4696728ea42d3f559b757a034d97260f.jpg"
+    }
+]
